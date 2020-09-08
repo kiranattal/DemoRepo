26362c2f5de702317c30988d1ad0b64514bcb6c4
Kiran Attal <attal.kiran@gmail.com>
2020-09-08 16:49:57 +0530

    second git commit for my selenium

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,14 @@
 package com.qa.pages;
 
 public class LoginPage {
+	
+	public void loginPage()
+	{
+		System.out.println("Login Page");
+	}
+	public void loginset()
+	{
+		System.out.println("login set method");
+	}
 
 }
